949c5a79c9f5dcb7bbf33eb5448d01b1451b5db8
mrheio <drg.ov00@gmail.com>
2024-04-10 10:38:01 +0300

    feat: Add Vercel Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
 	integrations: [react()],
 });
